19f0b25d4df413b3e961dac6d9a6f2eb1cfa2f95
packy <lpreterite@126.com>
2016-10-09 18:25:41 +0800

    add api propertie translate with sequelize model

--- example/app.js
+++ example/app.js
@@ -1,8 +1,12 @@
 var feathers = require('feathers');
 var rest = require('feathers-rest');
+var service = require('feathers-sequelize');
+var Sequelize = require('sequelize');
+var user = require('./user-model');
 var bodyParser = require('body-parser');
 var feathersSwagger = require('../lib');
 var pkg = require('../package.json');
+var path = require('path');
 
 var port = 3000;
 var app = feathers();
@@ -13,6 +17,13 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
  });
+
+// Set Sequelize
+app.set('sequelize', new Sequelize('sequelize','','', {
+    dialect: 'sqlite',
+    logging: false,
+    storage: path.join(__dirname, '../db.sqlite')
+}));
 
 // configure
 app
@@ -32,72 +43,81 @@ app
             'license': 'MIT',
             'licenseUrl': 'https://github.com/Glavin001/feathers-swagger/blob/master/LICENSE'
         }
-    }));
-
-app.use('/user', {
-    get: function(id, params){
-        return Promise.resolve({
-            id: 1,
-            name: 111
-        });
-    }
-})
-
-app.use('/pet', {
-    find: function(params) {
-        return Promise.resolve([]);
-    },
-    get: function(id, params) {
-        return Promise.resolve({});
-    },
-    create: function(data, params) {
-        return Promise.resolve({});
-    },
-    update: function(id, data, params) {
-        return Promise.resolve({});
-    },
-    remove: function(id, params) {
-        return Promise.resolve({});
-    },
-    setup: function(app) {
-        // console.log(app)
-    },
-    docs: {
-        description: 'Operations about examples.',
-        find: {
-            parameters: [{
-                name: 'name',
-                description: 'Filter Examples by name.',
-                required: false,
-                type: 'string',
-                paramType: 'formData'
-            }],
-            responses: [
-                {
-                    code: 500,
-                    reason: 'Example error.'
+    }))
+    .configure(function(){
+        var model = user(this.get('sequelize')),
+            options = {
+                Model: model,
+                paginate: {
+                    default: 5,
+                    max: 25
                 }
-            ]
-        },
-        models: {
-            Example: {
-                id: 'Example',
-                description: 'This is an Example model.',
-                required: ['name'],
-                properties: {
-                    name: { 
-                        type: 'string',
-                        description: 'This is the example name.'
-                    },
-                    anotherProperty: {
-                        type:'string',
-                        description: 'This is the example description.'
-                    }
-                }
-            }
-        }
-    }
-});
+            };
+
+        var doc = {
+            description: 'Operations about Users.',
+            definition: feathersSwagger.toModel(model)
+        };
+
+        // Initialize our service with any options it requires
+        this.use('/users', Object.assign(service(options), {docs: doc}));
+    });
+
+// app.use('/pet', {
+//     find: function(params) {
+//         return Promise.resolve([]);
+//     },
+//     get: function(id, params) {
+//         return Promise.resolve({});
+//     },
+//     create: function(data, params) {
+//         return Promise.resolve({});
+//     },
+//     update: function(id, data, params) {
+//         return Promise.resolve({});
+//     },
+//     remove: function(id, params) {
+//         return Promise.resolve({});
+//     },
+//     setup: function(app) {
+//         // console.log(app)
+//     },
+//     docs: {
+//         description: 'Operations about examples.',
+//         find: {
+//             parameters: [{
+//                 name: 'name',
+//                 description: 'Filter Examples by name.',
+//                 required: false,
+//                 type: 'string',
+//                 paramType: 'formData'
+//             }],
+//             responses: [
+//                 {
+//                     code: 500,
+//                     reason: 'Example error.'
+//                 }
+//             ]
+//         },
+//         models: {
+//             Example: {
+//                 id: 'Example',
+//                 description: 'This is an Example model.',
+//                 required: ['name'],
+//                 properties: {
+//                     name: { 
+//                         type: 'string',
+//                         description: 'This is the example name.'
+//                     },
+//                     anotherProperty: {
+//                         type:'string',
+//                         description: 'This is the example description.'
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// });
 
 
 app.listen(port, function(){
---
+++ example/user-model.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// user-model.js - A sequelize model
+// 
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+
+const Sequelize = require('sequelize');
+
+module.exports = function(sequelize) {
+  const user = sequelize.define('users', {
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false
+    }
+  }, {
+    freezeTableName: true
+  });
+
+  user.sync();
+
+  return user;
+};
