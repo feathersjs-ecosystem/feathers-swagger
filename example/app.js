0347441a50069878b75b3a9a1aab58c5d1e4efca
David Luecke <daff@neyeon.com>
2016-11-21 19:07:47 -0800

    support authorization setting and separation of docsPath and basePath (#17)
    * update swagger-ui to version 2.0
    
    * delete 1.2 code
    
    * add swagger-ui by 2.2.5
    
    * update example with feathers to 2.0.2
    
    * add api propertie translate with sequelize model
    
    * fix example code
    
    * up
    
    * up
    
    * update example
    
    * update Example and README
    
    * swagger-ui use NPM instead of bower
    
    * doc path and api base path may be different
    
    * add authorization in swagger setting and example
    
    * Support for full authorization

--- example/app.js
+++ example/app.js
@@ -3,6 +3,8 @@ var cors = require('cors');
 var feathers = require('feathers');
 var rest = require('feathers-rest');
 var service = require('feathers-sequelize');
+var hooks = require('feathers-hooks');
+var authentication = require('feathers-authentication');
 var Sequelize = require('sequelize');
 var user = require('./user-model');
 var bodyParser = require('body-parser');
@@ -30,9 +32,8 @@ app
     /* ===== Important: Feathers-Swagger part below ===== */
     // Use Feathers Swagger Plugin
     .configure(feathersSwagger({
-        docsPath:'docs',
+        docsPath:'/docs',
         version: pkg.version,
-        basePath: '',
         info: {
             'title': pkg.name,
             'description': pkg.description,
@@ -45,9 +46,62 @@ app
                 name: 'MIT',
                 'url': 'https://github.com/Glavin001/feathers-swagger/blob/master/LICENSE'
             }
+        },
+        security: {
+            'type': 'apiKey',
+            'name': 'authorization',
+            'in': 'header'
+        },
+        securityDefinitions: {
+            'authorization': {
+                'type': 'apiKey',
+                'name': 'authorization',
+                'in': 'header'
+            }
+        },
+        definitions: {
+            'local': {
+                type: 'object',
+                properties: {
+                    email: {
+                        type: 'string',
+                    },
+                    password: {
+                        type: 'string',
+                    }
+                }
+            },
+            'token': {
+                type: 'object',
+                properties: {
+                    token: {
+                        type: 'string',
+                    }
+                }
+            },
+            'paginate': new feathersSwagger.utils.Definition({
+                attributes: {
+                    total: Sequelize.INTEGER,
+                    limit: Sequelize.INTEGER,
+                    skip: Sequelize.INTEGER,
+                    data: Sequelize.ARRAY
+                }
+            })
         }
     }))
+    .configure(hooks())
     .configure(rest())
+    .configure(function(){
+        var app = this;
+        var config = {
+                'idField': 'id',
+                'token': {
+                    'secret': '7RJeSzXr2n/Mb15vl1lVAUs7PHNjvlV3ltJLpBjdJ93MPanV1HkFf5WjK/J4V2hqFaxALsPrqr7cgBPsA0M0DQ=='
+                },
+                'local': {}
+            };
+        app.configure(authentication(config));
+    })
     .configure(function(){
         // Add your service(s)
         var model = user(this.get('sequelize')),
@@ -62,29 +116,27 @@ app
         var doc = {
             description: 'Operations about Users.',
             definitions: {
-                paginate: new feathersSwagger.util.Definition({}, 'object', {
-                    total: new feathersSwagger.util.Propertie('INTEGER'),
-                    limit: new feathersSwagger.util.Propertie('INTEGER'),
-                    skip: new feathersSwagger.util.Propertie('INTEGER'),
-                    data: new feathersSwagger.util.Propertie('ARRAY', {
-                        '$ref': '#/definitions/users'
-                    })
-                })
+                'UserPaginate': new feathersSwagger.utils.Definition({
+                    attributes:{
+                        data: Sequelize.ARRAY('users') 
+                    }
+                }, { extends: ['paginate'] })
             },
-            definition: new feathersSwagger.util.Definition(model),
+            definition: new feathersSwagger.utils.Definition(model),
+            securities: ['find'],
             find: {
                 parameters: [{
                     description: 'Get examples by name',
-                    in: 'path',
-                    required: true,
-                    name: 'name',
+                    in: 'query',
+                    required: false,
+                    name: 'email',
                     type: 'string'
                 }],
                 responses: {
                     '200': {
                         description: 'successful operation',
                         schema: {
-                            '$ref': '#/definitions/paginate'
+                            '$ref': '#/definitions/UserPaginate'
                         }
                     }
                 }
@@ -93,6 +145,20 @@ app
 
         // Initialize our service with any options it requires
         this.use('/users', Object.assign(service(options), {docs: doc}));
+
+        const userService = this.service('/users');
+        const auth = authentication.hooks;
+        userService.before({
+            create: [auth.hashPassword()],
+            find: [
+                auth.verifyToken(),
+                auth.populateUser(),
+                auth.restrictToAuthenticated()
+            ]
+        });
+        userService.after({
+            all: [hooks.remove('password')]
+        });
     });
 
 
