409477cdfbb3e115e665e574db8796d49ef8d947
David Luecke <daff@neyeon.com>
2016-11-22 08:23:46 -0800

    Get plugin release ready (#18)
    * update swagger-ui to version 2.0
    
    * delete 1.2 code
    
    * add swagger-ui by 2.2.5
    
    * update example with feathers to 2.0.2
    
    * add api propertie translate with sequelize model
    
    * fix example code
    
    * up
    
    * up
    
    * update example
    
    * update Example and README
    
    * swagger-ui use NPM instead of bower
    
    * doc path and api base path may be different
    
    * add authorization in swagger setting and example
    
    * Support for full authorization
    
    * Get plugin release ready
    
    * Remove database
    
    * Some better docs

---
+++ example/simple/app.js
@@ -0,0 +1,46 @@
+const feathers = require('feathers');
+const rest = require('feathers-rest');
+const memory = require('feathers-memory');
+const path = require('path');
+const bodyParser = require('body-parser');
+const swagger = require('../../lib');
+
+const messageService = memory();
+
+messageService.docs = {
+  description: 'A service to send and receive messages',
+  definitions: {
+    messages: {
+      "type": "object",
+      "required": [
+        "text"
+      ],
+      "properties": {
+        "text": {
+          "type": "string",
+          "description": "The message text"
+        },
+        "useId": {
+          "type": "string",
+          "description": "The id of the user that send the message"
+        }
+      }
+    }
+  }
+};
+
+const app = feathers()
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }))
+  .configure(rest())
+  .configure(swagger({
+    docsPath: '/docs',
+    uiIndex: path.join(__dirname, 'docs.html'),
+    info: {
+      title: 'A test',
+      description: 'A description'
+    }
+  }))
+  .use('/messages', messageService);
+
+app.listen(3030);
--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,44 @@
-var assert = require('assert');
+import { expect } from 'chai';
+import feathers from 'feathers';
+import rest from 'feathers-rest';
+import memory from 'feathers-memory';
+import rp from 'request-promise';
+import swagger from '../src';
 
-describe('name_test.js test', function () {
-    it('does something', function () {
-        assert.ok(true, 'Done!');
+describe('feathers-swagger', () => {
+  it('is CommonJS compatible', () => {
+    expect(typeof require('../lib')).to.equal('function');
+  });
+
+  describe('basic functionality', () => {
+    let server;
+
+    before(done => {
+      const app = feathers()
+        .configure(rest())
+        .configure(swagger({
+          docsPath: '/docs',
+          info: {
+            'title': 'A test',
+            'description': 'A description'
+          }
+        }))
+        .use('/messages', memory());
+
+      server = app.listen(6776, () => done());
     });
+
+    after(done => server.close(done));
+
+    it('supports basic functionality with a simple app', () => {
+      return rp({
+        url: 'http://localhost:6776/docs',
+        json: true
+      }).then(docs => {
+        expect(docs.info.title).to.equal('A test');
+        expect(docs.info.description).to.equal('A description');
+        expect(docs.paths['/messages']).to.exist;
+      });
+    });
+  });
 });
