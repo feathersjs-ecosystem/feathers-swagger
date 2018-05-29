ed690b0336f87aed4e3b6398cca09520a92dae4a
Andy <andys8@users.noreply.github.com>
2018-05-29 13:18:06 +0200

    Config contains idType to set default for application

--- test/index.test.js
+++ test/index.test.js
@@ -16,13 +16,16 @@ describe('feathers-swagger', () => {
     before(done => {
       const app = feathers()
         .configure(rest())
-        .configure(swagger({
-          docsPath: '/docs',
-          info: {
-            'title': 'A test',
-            'description': 'A description'
-          }
-        }))
+        .configure(
+          swagger({
+            docsPath: '/docs',
+            info: {
+              title: 'A test',
+              description: 'A description'
+            },
+            idType: 'string'
+          })
+        )
         .use('/messages', memory());
 
       server = app.listen(6776, () => done());
@@ -38,6 +41,16 @@ describe('feathers-swagger', () => {
         expect(docs.info.title).to.equal('A test');
         expect(docs.info.description).to.equal('A description');
         expect(docs.paths['/messages']).to.exist;
+      });
+    });
+
+    it('supports id types in config', () => {
+      return rp({
+        url: 'http://localhost:6776/docs',
+        json: true
+      }).then(docs => {
+        const messagesIdParam = docs.paths['/messages/{id}'].get.parameters[0];
+        expect(messagesIdParam.type).to.equal('string');
       });
     });
   });
